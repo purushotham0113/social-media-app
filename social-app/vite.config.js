c76c77c70fbf6448c16728b34fa50b462d214f82
purushotham0113 <229x1a33b3@gmail.com>
2025-11-04 15:54:44 +0530

    changes

--- social-app/vite.config.js
+++ social-app/vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: '_redirects',
-          dest: '' // copy to dist root
-        }
-      ]
-    })
-  ]
+  plugins: [react()],
 })
